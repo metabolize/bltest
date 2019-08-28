c5c4e1e4d727bf289257c1bfdf358578ab33c2f5
Paul Melnikow <email@paulmelnikow.com>
2019-08-28 12:42:39 -0400

    RELEASE 1.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 1.1.1
+
+- Use forked dependencies.
+
+## 1.1.0
+
+Initial publish of fork.
--- bltest/__init__.py
+++ bltest/__init__.py
@@ -1,6 +1,6 @@
 import contextlib
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 def attr(*args, **kwargs):
     '''
